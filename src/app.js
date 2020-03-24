6e2c8405d92cb267d5a6a4697c812002df26981b
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-24 10:48:10 -0500

    set up react router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'sanitize.css/sanitize.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
+import { Route } from 'react-router-dom';
 import GlobalStyle from '../../global-styles';
+
+const Home = () => <h1>Hello, world!</h1>;
+const LogIn = () => <h1>Login</h1>;
+const Register = () => <h1>Register</h1>;
 
 function App() {
   return (
-    <React.StrictMode>
+    <StrictMode>
       <div className="app">
-        <h1>Hello World</h1>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/" component={Home} />
       </div>
       <GlobalStyle />
-    </React.StrictMode>
+    </StrictMode>
   );
 }
 
