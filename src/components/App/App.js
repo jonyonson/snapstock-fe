1316b716ea68f1159ebd88d90d5fb3b280eeeb81
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-24 10:59:50 -0500

    set up router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,17 @@
 import React, { StrictMode } from 'react';
 import { Route } from 'react-router-dom';
 import GlobalStyle from '../../global-styles';
+import SignIn from '../SignIn';
+import SignUp from '../SignUp';
 
 const Home = () => <h1>Hello, world!</h1>;
-const LogIn = () => <h1>Login</h1>;
-const Register = () => <h1>Register</h1>;
 
 function App() {
   return (
     <StrictMode>
       <div className="app">
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
         <Route exact path="/" component={Home} />
       </div>
       <GlobalStyle />
