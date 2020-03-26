ebe78cf4906cc361dd1e37ddce125ffe1b7f6f44
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-25 19:21:43 -0500

    remove the news component for now

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,11 @@ import React, { useState, Fragment, useEffect } from 'react';
 import SearchBar from './search-bar';
 import StockQuote from './stock-quote';
 import StockChart from './stock-chart';
-import CompanyNews from './company-news';
 import axios from 'axios';
 
 function App() {
   const [selection, setSelection] = useState(null);
   const [quote, setQuote] = useState(null);
-  const [news, setNews] = useState([]);
   const [chart, setChart] = useState([]);
   const [logoURL, setLogoURL] = useState(null);
 
@@ -22,7 +20,6 @@ function App() {
           console.log(res.data);
           setQuote(res.data.quote);
           setLogoURL(res.data.logo.url);
-          setNews(res.data.news);
           setChart(res.data['intraday-prices'] || res.data.chart);
         })
         .catch((err) => {
@@ -41,7 +38,6 @@ function App() {
       <SearchBar onSelect={handleSearchSelection} />
       <StockQuote logoURL={logoURL} quote={quote} />
       <StockChart data={chart} />
-      {false && <CompanyNews news={news} />}
     </Fragment>
   );
 }
