98a6d17e972f87bc16745342d411aa5fca86f0bb
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-29 12:49:03 -0500

    adjust font size of signup/signin text

--- src/styles/auth.styled.js
+++ src/styles/auth.styled.js
@@ -2,18 +2,19 @@ import styled from 'styled-components';
 
 const AuthWrapper = styled.div`
   max-width: 400px;
-  /* border: 4px solid gray; */
   border-radius: 2px;
-  /* margin: 10% auto 0; */
   padding: 1rem;
+  /* border: 4px solid gray; */
+  /* margin: 10% auto 0; */
 
   .signup-link,
   .signin-link {
+    font-size: 1.1rem;
     text-decoration: none;
     color: ${(props) => props.theme.colors.secondary};
 
     &:hover {
-      border-bottom: 1px solid currentColor;
+      border-bottom: 2px solid currentColor;
     }
   }
 `;
