739db0731bf440b7c1f3503fbfb074472c88df6b
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-29 12:59:15 -0500

    signin form ready to send data

--- src/components/sign-in.js
+++ src/components/sign-in.js
@@ -1,14 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import AuthWrapper from '../styles/auth.styled';
 import Header from './header';
 
 function SignIn() {
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
+
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(credentials);
+  };
+
   return (
     <Fragment>
       <Header />
       <AuthWrapper>
         <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="email"
+            onChange={handleChange}
+            value={credentials.email}
+          />
+          <input
+            name="password"
+            onChange={handleChange}
+            value={credentials.password}
+          />
+          <button type="submit">Sign In</button>
+        </form>
         <Link className="signup-link" to="/signup">
           Create an account
         </Link>
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,7 @@ export default {
     primary: '#044B7F', // header
     secondary: '#12939a', // chart stroke
     accent: '#EF476F', // range buttons
+
     gain: 'green',
     loss: 'red',
 
