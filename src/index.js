0522c20b5c3eacb590fe92f4bb6563033546c69f
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-29 21:53:22 -0500

    organize routes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import App from './components/app';
-import GlobalStyle from './styles/global-styles';
 import SignIn from './components/sign-in';
 import SignUp from './components/sign-up';
 import Watchlist from './components/watchlist';
+import GlobalStyle from './styles/global-styles';
 import 'sanitize.css/sanitize.css';
 
 ReactDOM.render(
@@ -15,9 +15,9 @@ ReactDOM.render(
     <ThemeProvider theme={theme}>
       <div className="app">
         <Switch>
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/signin" component={SignIn} />
           <Route exact path="/" component={App} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} />
           <Route path="/stocks/:symbol" component={App} />
           <Route path="/watchlist" component={Watchlist} />
         </Switch>
