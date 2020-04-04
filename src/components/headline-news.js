f0ab3aecbf38a52362f283f8e9efd59f3317173a
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-04-04 17:41:03 -0500

    show most recent story image and link

--- src/components/headline-news.js
+++ src/components/headline-news.js
@@ -33,11 +33,28 @@ function NewsHeadlines() {
       });
   }, []);
 
+  const mostRecentHeadline =
+    headlines.length > 0
+      ? headlines[0].title.slice(0, headlines[0].title.lastIndexOf('-') - 1)
+      : '';
+
   return (
     <Section>
+      {headlines.length > 0 && (
+        <a href={headlines[0].url} className="most-recent-story">
+          <div className="most-recent-story__headline">
+            {mostRecentHeadline}
+          </div>
+          <img
+            className="most-recent-story__image"
+            src={headlines[0].urlToImage}
+            alt=""
+          />
+        </a>
+      )}
       <div className="section-title">Latest News</div>
       {headlines
-        .filter((_, index) => index <= 4)
+        .filter((_, index) => index > 0 && index <= 4)
         .map((story) => {
           return (
             <div key={story.url} className="article">
@@ -58,6 +75,27 @@ const Section = styled.section`
   margin-top: 2rem;
   margin-bottom: 2rem;
   font-size: 0.875rem;
+
+  .most-recent-story {
+    &__headline {
+      font-size: 20px;
+      line-height: 1.1;
+      margin-bottom: 1rem;
+    }
+
+    &__image {
+      max-width: calc(100% + 2rem);
+      margin-bottom: 2rem;
+      margin-left: -1rem;
+      /* margin-right: -1rem; */
+
+      @media (min-width: 415px) {
+        max-width: 100%;
+
+        margin-left: 0;
+      }
+    }
+  }
 
   @media (min-width: 500px) {
     font-size: 0.9375rem;
