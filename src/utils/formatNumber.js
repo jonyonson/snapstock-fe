fddd2d6d7d0413fa5af803a0f39e4b2595d0d797
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-04-08 13:20:31 -0500

    add billion and million symbols

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,18 +1,22 @@
-function formatNumber(num, symbol = null, change = false) {
+function formatNumber(num, symbol = null, change = false, decimalPlaces = 2) {
   const isFloat = Number(num) % 1 !== 0;
 
   let number = Number(num)
-    .toFixed(2)
+    .toFixed(decimalPlaces)
     .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 
   if (!isFloat) number = number.split('.')[0];
 
-  if (symbol === '%') {
-    number += '%';
+  // if number is a percent or a number presented in ...
+  // billions (B) or millions (M) then add symbol to end
+  // else symbol is a currency symbol and should be added to beginning
+  if (symbol === '%' || symbol === 'B' || symbol === 'M') {
+    number += symbol;
   } else {
     number = symbol ? symbol + number : number;
   }
 
+  // show the '+' sign to represent change
   if (change) {
     number = Number(num) > 0 ? `+${number}` : number;
   }
