0427a5578aa68957f6366d02bcfae83f35542d60
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-04-12 11:46:09 -0500

    add stylng for info and error alerts

--- src/components/common/alert.js
+++ src/components/common/alert.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Alert({ children }) {
-  return <StyledAlert>{children}</StyledAlert>;
+function Alert({ type, children }) {
+  return <StyledAlert type={type}>{children}</StyledAlert>;
 }
 
 const StyledAlert = styled.div`
-  background-color: ${(props) => props.theme.colors.info};
-  border: 1px solid ${(props) => props.theme.colors.infoBorder};
+  width: 100%;
+  color: ${(props) => {
+    if (props.type === 'info') {
+      return props.theme.colors.infoText;
+    } else if (props.type === 'error') {
+      return props.theme.colors.errorText;
+    }
+  }};
+
+  background-color: ${(props) => {
+    if (props.type === 'info') {
+      return props.theme.colors.infoBackground;
+    } else if (props.type === 'error') {
+      return props.theme.colors.errorBackground;
+    }
+  }};
+
+  border: 1px solid
+    ${(props) => {
+      if (props.type === 'info') {
+        return props.theme.colors.infoBorder;
+      } else if (props.type === 'error') {
+        return props.theme.colors.errorBorder;
+      }
+    }};
+
   font-size: 0.875rem;
   line-height: 1.5;
-  color: rgba(0, 0, 0, 0.65);
   border-radius: 2px;
   padding: 0.5rem 0.9375rem;
+
+  &:not(:first-of-type) {
+    margin-top: 1rem;
+  }
 
   a {
     font-weight: bold;
