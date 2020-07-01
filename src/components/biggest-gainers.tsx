23e1fe5df009d335eeee18c62bd0ad49bf760dda
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-30 21:20:49 -0500

    convert to stock lists to ts and add error handling

--- src/components/biggest-gainers.tsx
+++ src/components/biggest-gainers.tsx
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import StockList from './common/stock-list';
 
 import { BASE_API_URL } from '../constants';
 
 function BiggestLosers() {
   const [biggestGainers, setBiggestGainers] = useState([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleError = (e: AxiosError) => {
+    console.error(e);
+    setError('Error fetching data.');
+  };
 
   useEffect(() => {
-    const url = `${BASE_API_URL}/api/stocks/market/list/gainers`;
     axios
-      .get(url)
-      .then((res) => {
-        setBiggestGainers(res.data);
-      })
-      .catch((err) => {
-        // TODO: handle errors
-        console.error(err);
-      });
+      .get(BASE_API_URL + '/api/stocks/market/list/gainers')
+      .then((res) => setBiggestGainers(res.data))
+      .catch((err: AxiosError) => handleError(err));
   }, []);
 
   return (
@@ -27,12 +27,19 @@ function BiggestLosers() {
         <span>Biggest Gainers</span>
       </div>
       <StockList stockList={biggestGainers} displayLength={5} />
+      {error && <Error>{error}</Error>}
     </Section>
   );
 }
 
 const Section = styled.section`
   margin-bottom: 2rem;
+`;
+
+const Error = styled.p`
+  color: red;
+  margin-top: 1rem;
+  font-size: 90%;
 `;
 
 export default BiggestLosers;
