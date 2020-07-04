c8ac8747c1a5861738acba205d36f3455e3f0ea0
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-07-04 15:54:23 -0500

    cleanup

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -5,7 +5,6 @@ import SignIn from '../containers/sign-in';
 import SignUp from '../containers/sign-up';
 import Watchlist from '../containers/watchlist';
 import GlobalStyle from '../styles/global-styles';
-// import MostActive from './most-active';
 import SymbolPage from '../containers/symbol';
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
       <Route path="/stocks/:symbol" component={SymbolPage} />
       <Route path="/signup" component={SignUp} />
       <Route path="/signin" component={SignIn} />
-      {/* <Route path="/active" component={MostActive} /> */}
       <GlobalStyle />
     </Fragment>
   );
