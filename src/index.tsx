10a8576c1a9f9489691477688da6fa1787dbad9a
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-07-05 23:28:22 -0500

    remove service worker file

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,12 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import App from './components/app';
 import 'sanitize.css/sanitize.css';
-import * as serviceWorker from './utils/serviceWorker';
 
 ReactDOM.render(
-  // <React.StrictMode>
   <BrowserRouter>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </BrowserRouter>,
-  // </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
