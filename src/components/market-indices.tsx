7bf9b2582513f5e5d887a1ad03b84ddba1e4b8bd
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-07-06 15:12:59 -0500

    break stock widget component out

--- src/components/market-indices.tsx
+++ src/components/market-indices.tsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import formatNumber from '../utils/formatNumber';
 import useInterval from '../hooks/use-interval';
 import { BASE_API_URL } from '../constants';
-
-interface Index {
-  change: number;
-  percentChange: number;
-  price: number;
-}
-
-interface Props {
-  name: 'DOW' | 'NASDAQ' | 'S&P 500';
-  index: Index | null;
-}
+import Widget from './index-widget';
 
 export default function MarketIndices() {
   const [dow, setDow] = useState(null);
@@ -48,91 +37,4 @@ export default function MarketIndices() {
 const StyledContainer = styled.div`
   display: flex;
   justify-content: space-between;
-`;
-
-const Widget = ({ name, index }: Props) => {
-  return (
-    <StyledWidget index={index} name={name}>
-      <div className="top">
-        <div>{name}</div>
-        <div className="change">
-          {index ? formatNumber(index.change, { change: true }) : '--'}
-        </div>
-      </div>
-
-      <div className="bottom">
-        <div className="percent-change">
-          <span>
-            {index ? formatNumber(index.percentChange, { change: true }) : '--'}
-          </span>
-        </div>
-
-        <div className="price">{index ? formatNumber(index.price) : '--'}</div>
-      </div>
-    </StyledWidget>
-  );
-};
-
-const StyledWidget = styled.div<Props>`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 50%;
-  padding: 0.4rem;
-  font-size: 0.6875rem;
-  font-weight: 700;
-  color: white;
-  border: 2px solid transparent;
-  transition: background-color 0.3s ease-in;
-
-  @media (min-width: 375px) {
-    width: 33%;
-    font-size: 0.625rem;
-    padding: 0.2rem;
-  }
-
-  @media (min-width: 400px) {
-    font-size: 0.6875rem;
-    padding: 0.4rem;
-  }
-
-  &:nth-of-type(1) {
-    margin-right: 0.25rem;
-  }
-
-  &:nth-of-type(2) {
-    margin-left: 0.25rem;
-    @media (min-width: 375px) {
-      margin-right: 0.25rem;
-    }
-  }
-
-  &:nth-of-type(3) {
-    display: none;
-    @media (min-width: 375px) {
-      display: flex;
-      margin-left: 0.25rem;
-    }
-  }
-
-  color: ${(props) =>
-    props.index !== null ? props.theme.colors.white : props.theme.colors.black};
-
-  background-color: ${(props) =>
-    props.index !== null
-      ? props.index.percentChange >= 0
-        ? props.theme.colors.gain
-        : props.theme.colors.loss
-      : 'rgba(0,0,0,0.2)'};
-
-  .top,
-  .bottom {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .top {
-    margin-bottom: 5px;
-  }
 `;
