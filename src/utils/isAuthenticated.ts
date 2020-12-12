0990dc7fe416ed829e95e6ebeb0c2db71fdc9417
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-12-12 16:31:11 -0600

    add a check for expired token

--- src/utils/isAuthenticated.ts
+++ src/utils/isAuthenticated.ts
@@ -1,3 +1,17 @@
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+
+function isTokenExpired(exp: number | undefined) {
+  if (!exp) return false;
+  return Date.now() >= exp * 1000;
+}
+
 export default function isAuthenticated() {
-  return Boolean(window.localStorage.getItem('token'));
+  if (window.localStorage.getItem('token')) {
+    const token = window.localStorage.getItem('token') || '';
+    const decoded = jwtDecode<JwtPayload>(token);
+
+    return !isTokenExpired(decoded.exp);
+  }
+
+  return false;
 }
