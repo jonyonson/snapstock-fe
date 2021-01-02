1399100fca1a18b1d39a0afb6e2f93615897d9e3
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-01-02 14:29:14 -0600

    refactor: folder structure

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
-import Home from '../containers/home';
-import SignIn from '../containers/sign-in';
-import SignUp from '../containers/sign-up.js';
-import Watchlist from '../containers/watchlist';
+import Home from '../pages/home';
+import SignIn from '../pages/sign-in';
+import SignUp from '../pages/sign-up.js';
+import Watchlist from '../pages/watchlist';
 import GlobalStyle from '../styles/global-styles';
-import SymbolPage from '../containers/symbol';
+import SymbolPage from '../pages/symbol';
 
 function App() {
   return (
