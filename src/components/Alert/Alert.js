c229976dd2e36d9f60ffbcb9856ef7b24ae3c774
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-01-16 12:36:54 -0600

    chore: update gitignore

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -4,12 +4,16 @@ import PropTypes from 'prop-types';
 import './Alert.scss';
 
 function Alert({ severity, children }) {
-  return <div className={`Alert-root Alert-${severity}`}>{children}</div>;
+  return (
+    <div className={`Alert-root Alert-${severity}`} data-testid="alert">
+      {children}
+    </div>
+  );
 }
 
 Alert.propTypes = {
   severity: PropTypes.oneOf(['error', 'info', 'success', 'warning']).isRequired,
-  children: PropTypes.element.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default Alert;
