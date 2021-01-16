2831ca70cfee9d259e59b59d7e2282b65dd6fe30
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-01-16 13:17:45 -0600

    test: adds tests for Alert

---
+++ src/components/Alert/Alert.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Alert from './Alert';
+
+it('renders a warning alert with correct class names', () => {
+  render(<Alert severity='warning'>Warning!</Alert>);
+  const element = screen.getByTestId('alert');
+  expect(element).toHaveClass('Alert-root')
+  expect(element).toHaveClass('Alert-warning')
+});
+
+it('renders an info alert with correct class names', () => {
+  render(<Alert severity='info'>Info</Alert>);
+  const element = screen.getByTestId('alert');
+  expect(element).toHaveClass('Alert-root')
+  expect(element).toHaveClass('Alert-info')
+});
+
+it('renders an error alert with correct class names', () => {
+  render(<Alert severity='error'>Error</Alert>);
+  const element = screen.getByTestId('alert');
+  expect(element).toHaveClass('Alert-root')
+  expect(element).toHaveClass('Alert-error')
+});
+
+it('renders a success alert with correct class names', () => {
+  render(<Alert severity='success'>Success</Alert>);
+  const element = screen.getByTestId('alert');
+  expect(element).toHaveClass('Alert-root')
+  expect(element).toHaveClass('Alert-success')
+})
