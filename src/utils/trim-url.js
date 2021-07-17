99983d4945b8bde011bfc9a98c59849029200803
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-07-17 14:47:02 -0500

    fix: return empty string if no url provided

--- src/utils/trim-url.js
+++ src/utils/trim-url.js
@@ -1,4 +1,5 @@
 export default function trimUrl(url) {
+  if (!url) return '';
   url = url.replace(/(^\w+:|^)\/\//, ''); // remove `http(s)://`
   url = url.replace('www.', ''); // remove www.
   url = url.replace(/\/$/, ''); // remove trailing slash
