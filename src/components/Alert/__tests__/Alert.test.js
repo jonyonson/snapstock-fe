0aecb4f4f8b6eb3d30e45ab15a22d446b7841933
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-07-25 06:10:19 -0500

    fix: import error on alert tests

--- src/components/Alert/__tests__/Alert.test.js
+++ src/components/Alert/__tests__/Alert.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import Alert from './Alert';
+import Alert from '../Alert';
 
 describe('Alert', () => {
   it('renders a warning alert with correct class names', () => {
