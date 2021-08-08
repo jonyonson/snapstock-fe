12ba543298bda16dee86a1e7036ab598fadc4492
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-08-08 07:47:14 -0500

    feat: update modal styles

--- src/components/Modal/Modal.stories.js
+++ src/components/Modal/Modal.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { showModal, closeModal } from '../../utils/modal';
-
+import Button from '../Button';
 import Modal from './Modal';
 
 export default {
@@ -21,8 +21,25 @@ const Template = (args) => {
     <>
       <button onClick={showTheDangModal}>Trigger</button>
       <Modal {...args}>
-        <div>I am a modal</div>
-        <button onClick={() => closeModal('modal-1')}>Close</button>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+          harum nulla consequatur quos dolorum inventore, deleniti error quis
+          voluptatum numquam molestiae exercitationem ipsum animi labore
+          possimus ea neque. Illum, qui.
+        </div>
+        <Button
+          variant="primary"
+          label="OK"
+          onClick={() => {
+            alert('success');
+            closeModal('modal-1');
+          }}
+        />
+        <Button
+          variant="secondary"
+          onClick={() => closeModal('modal-1')}
+          label="Cancel"
+        />
       </Modal>
     </>
   );
@@ -31,5 +48,5 @@ const Template = (args) => {
 export const Default = Template.bind({});
 Default.args = {
   id: 'modal-1',
-  title: 'Default Modal',
+  title: 'This is a Modal Title',
 };
